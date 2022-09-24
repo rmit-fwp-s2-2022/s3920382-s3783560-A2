683b79b8f1d12d0f18063edd530c082a05cd5f00
Nick-jw <s3920382@student.rmit.edu.au>
2022-09-25 02:50:02 +1000

    layout

--- react/src/fragments/Footer.js
+++ react/src/fragments/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto py-3 bg-dark text-white">
+    <footer className="mt-auto py-3 bg-dark text-white" style={{margin:0}}>
       <div className="container">Register, Login, Logout and Post Example &copy; 2022</div>
     </footer>
   );
--- react/src/pages/Home.js
+++ react/src/pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
 import logo from "../logo.svg";
+import "../index.css"
 
 export default function Home(props) {
   return (
     <div className="text-center">
-      <h1 className="display-4">Home</h1>
+      <h1 className="display-4 main-font">Home</h1>
       {props.user !== null && <h4><strong>Hello {props.user.first_name} {props.user.last_name}!</strong></h4>}
       <img src={logo} className="w-50" alt="logo" />
     </div>
