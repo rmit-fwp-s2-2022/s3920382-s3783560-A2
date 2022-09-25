b1523abe0330fd48accf957f62c02f6b0b98ddd3
Nick-jw <s3920382@student.rmit.edu.au>
2022-09-25 23:03:42 +1000

    footer styling

--- react/src/fragments/Footer.js
+++ react/src/fragments/Footer.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../index.css"
 
 export default function Footer() {
   return (
-    <footer className="mt-auto py-3 bg-dark text-white" style={{margin:0}}>
+    <footer className="mt-auto py-3 footer">
       <div className="container">Register, Login, Logout and Post Example &copy; 2022</div>
     </footer>
   );
