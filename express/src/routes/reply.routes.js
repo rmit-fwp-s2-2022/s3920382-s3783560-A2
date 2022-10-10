153ee65a6c834dfbcc4475ef242373cf03756eed
RickyKT <s3783560@student.rmit.edu.au>
2022-10-10 12:11:40 +1100

    deletes select all route

--- express/src/routes/reply.routes.js
+++ express/src/routes/reply.routes.js
@@ -2,9 +2,6 @@ module.exports = (express, app) => {
     const controller = require("../controllers/reply.controller.js");
     const router = express.Router();
   
-    // Select all reply.
-    router.get("/", controller.all);
-  
     // Create a new reply.
     router.post("/", controller.create);
   
