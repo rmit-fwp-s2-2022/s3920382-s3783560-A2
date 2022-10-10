44414f4fbbeb36bf2f851760399328382d5b2a12
RickyKT <s3783560@student.rmit.edu.au>
2022-10-10 12:09:17 +1100

    eager loading the selct all posts

--- express/src/controllers/post.controller.js
+++ express/src/controllers/post.controller.js
@@ -1,13 +1,9 @@
 const db = require("../database");
 
-// Select all posts from the database.
+// Select all posts from the database. and respectively replies
 exports.all = async (req, res) => {
-  const posts = await db.post.findAll();
+  const posts = await db.post.findAll( {include:'reply'});
 
-  // Can use eager loading to join tables if needed, for example:
-  // const posts = await db.post.findAll({ include: db.user });
-
-  // Learn more about eager loading here: https://sequelize.org/master/manual/eager-loading.html
 
   res.json(posts);
 };
