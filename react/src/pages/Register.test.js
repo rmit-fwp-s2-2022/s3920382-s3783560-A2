5ca991100e686462df4dc99ccf7c0fa71b6b12e4
RickyKT <s3783560@student.rmit.edu.au>
2022-10-16 17:12:36 +1100

    checking on input fields

--- react/src/pages/Register.test.js
+++ react/src/pages/Register.test.js
@@ -1,4 +1,39 @@
 import {render, fireEvent,screen} from '@testing-library/react';
-import Login from './Login'
+import Register from './Register'
 import '@testing-library/jest-dom'
 import { createUser } from '../data/repository';
+import { BrowserRouter as Router } from "react-router-dom"
+
+
+let container;
+
+beforeEach(() => {
+    // eslint-disable-next-line testing-library/no-render-in-setup
+    const utils = render(
+        <Router>
+            <Register />
+        </Router>)
+    container = utils.container
+})
+
+test('checking of input input fields',() => {
+    // eslint-disable-next-line testing-library/no-node-access
+    let form = container.querySelector('form')
+    const email = screen.getByLabelText('Email')
+    const password = screen.getByLabelText('Password must be at least 6 characters')
+    const confirm = screen.getByLabelText('Confirm password')
+    const first = screen.getByLabelText('First name')
+    const last = screen.getByLabelText('Last name')
+
+    fireEvent.change(email,{target: {value: 'qwe@rmit'}});
+    fireEvent.change(password, {target: {value : '123456'}});
+    fireEvent.change(first, {target: {value: 'Ricky'}});
+    fireEvent.change(last, {target: {value: 'Truong'}})
+    fireEvent.change(confirm, {target: {value: '123456'}})
+
+    expect(email.value).toBe('qwe@rmit')
+    expect(password.value).toBe('123456')
+    expect(confirm.value).toBe('123456')
+    expect(first.value).toBe('Ricky')
+    expect(last.value).toBe('Truong')
+})
